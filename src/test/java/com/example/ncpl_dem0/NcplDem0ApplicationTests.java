fbea6a6342a061c01e3770db257884453a9830ad
Srinu-rj <dnsrinu143@gmail.com>
2025-01-24 08:51:04 +0530

    Initial commit

---
+++ src/test/java/com/example/ncpl_dem0/NcplDem0ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ncpl_dem0;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NcplDem0ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
