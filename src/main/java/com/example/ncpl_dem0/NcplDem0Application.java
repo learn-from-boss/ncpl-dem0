8ecdf3f03def1ca7b60e14d745715493389df409
srinu-rj <dnsrinu143@gmail.com>
2025-05-06 07:38:25 +0530

    new thing

--- src/main/java/com/example/ncpl_dem0/NcplDem0Application.java
+++ src/main/java/com/example/ncpl_dem0/NcplDem0Application.java
@@ -3,6 +3,7 @@ package com.example.ncpl_dem0;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,13 +11,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/main")
 public class NcplDem0Application {
-//localhost:9988/main/home
-	public static void main(String[] args) {
-		SpringApplication.run(NcplDem0Application.class, args);
-	}
+    //localhost:9988/main/home
+    public static void main(String[] args) {
+        SpringApplication.run(NcplDem0Application.class, args);
+    }
 
-	@GetMapping("/home")
-	public String homePage(){
-		return "sdlfhvaspdiuhfpo'[jfcdausidfsjdlfcsap]jfsdpjfsdf";
-	}
+    @GetMapping("/home")
+    public String homePage() {
+        return "sdlfhvaspdiuhfpo'[jfcdausidfsjdlfcsap]jfsdpjfsdf";
+    }
+
+    @GetMapping("/demo1")
+    public String demo(){
+        return "Version:2.0";
+    }
 }
